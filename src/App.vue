ad628420816f9888bb3bd1c49df0a31ba85859e6
Nigel Christiaans <nigelchristiaans747@gmail.com>
2023-04-23 20:57:11 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import WordCounter from './components/word-counter.vue'
+</script>
+
+<template>
+      <WordCounter/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/word-counter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="word-counter">
+    <h1>Word Counter</h1>
+    <p>
+      <textarea v-model="text" placeholder="Type here..."></textarea>
+    </p>
+    <p>
+      <strong>Word count:</strong> {{ wordCount }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "word-counter",
+  data() {
+    return {
+      text: "",
+      wordCount: 0,
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
